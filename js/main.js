596dea0ff6bd27d49f9c3af2212ee290fc8bd4a1
Belgotech <Belovedobiora2002@gmail.com>
2023-03-10 14:50:29 +0100

    APP IS WORKING`

--- js/main.js
+++ js/main.js
@@ -1,3 +1,21 @@
-const Input = document.querySelector('.input')
-const Test = document.querySelector('.text')
-const Button = document.querySelector('button')
+const StorageInput = document.querySelector('.input')
+const Text = document.querySelector('.text')
+const SaveButton = document.querySelector('button')
+
+StorageInput.addEventListener('input', written => {
+    Text.textContent = written.target.value
+})
+
+const saveToLocalStorage = () => {
+    localStorage.setItem('plainText', Text.textContent)
+}
+
+SaveButton.addEventListener('click', saveToLocalStorage)
+
+const plainText = localStorage.getItem('plainText')
+
+if (plainText !== null){
+ Text.textContent = plainText
+} else{
+    console.log('nothing found in local storage')
+}
